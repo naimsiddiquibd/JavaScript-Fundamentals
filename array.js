138560f8b19b9ba6fe43fe473b7ea9b1347e4c03
naimsiddiquibd <naimsiddiquibd@gmail.com>
2021-08-02 12:09:36 +0600

    2nd commit

---
+++ array.js
@@ -0,0 +1,26 @@
+// Declear an array
+var friendsAge = [11,13,12,14,17];
+var fruitsName = ['Apple','Banana','Mango'];
+console.log(friendsAge);
+
+// Count array's elements
+var vowels = ['a','e','i','o','u'];
+var vowelsCount = vowels.length;
+console.log(vowelsCount);
+
+// Array index
+var petName = ['Cat','Dog','Tiger','Lion','Enaconda'];
+var lionIndex = petName.indexOf('Lion');
+console.log(lionIndex);
+
+var Number = [3,5,3,7,8,5];
+var index = Number.indexOf(8);
+console.log(index);
+
+var Number = [3,5,3,7,8,5];
+var index = Number.indexOf(9);
+console.log(index);
+
+var Number = [3,5,3,7,8,5];
+var secondIndex = Number[3];
+console.log(secondIndex);
