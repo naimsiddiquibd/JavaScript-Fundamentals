515d130a38e6fc4d595bbda384e1b26051d3cc73
naimsiddiquibd <naimsiddiquibd@gmail.com>
2021-08-02 11:38:15 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,15 @@
+// Number
+var watchPrice = 120;
+watchPrice = 125;
+console.log(watchPrice);
+
+// String
+var userName = 'Naim Siddiqui';
+console.log(userName);
+
+// Boolean
+var isExpensive = true;
+console.log(isExpensive);
+
+// Integer: 1 2 3 4 5
+// Float: 2.432 4.323 8.434
