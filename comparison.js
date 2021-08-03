9f7ef39c7249fdd39962067a8f389d7532ef53fa
naimsiddiquibd <naimsiddiquibd@gmail.com>
2021-08-03 14:49:35 +0600

    conditional

--- comparison.js
+++ comparison.js
@@ -1,2 +1,15 @@
 // Greater-than check:
 console.log(5>6);
+
+// Smaller-than check:
+console.log(5<6);
+
+// Equal check:
+console.log(5==6);
+
+// Not Equal check:
+console.log(5!=6);
+
+// Double
+// console.log(studyGood && goodPerson)
+// console.log(isBanker || studyGood)
---
+++ conditional.js
@@ -0,0 +1,15 @@
+// If food is isFoodReady, I will eat
+var isFoodReady = true;
+if (isFoodReady = true) {
+    console.log('I will eat now');
+}
+
+// Iphone buying
+var iPhonePrice = 1100;
+var myBudget = 700;
+if (iPhonePrice<myBudget) {
+    console.log('I can buy it!');
+}
+else{
+    console.log('Xiaomi is better.');
+}
