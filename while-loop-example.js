af387f42a2b9eaad760b80af4a2d02d2913bf88c
naimsiddiquibd <naimsiddiquibd@gmail.com>
2021-08-04 13:04:11 +0600

    while-loop-example

---
+++ while-loop-example.js
@@ -0,0 +1,19 @@
+var number = 0; //Loop Variable
+while(number <= 10){
+    console.log(number);
+    number++;
+}
+
+// Odd Numbers
+var number = 1;
+while(number <= 20){
+    console.log(number);
+    number = number + 2;
+}
+
+// Even Numbers
+var number = 0;
+while(number <= 20){
+    console.log(number);
+    number = number + 2;
+}
