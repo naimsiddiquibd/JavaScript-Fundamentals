adb949b10d93acca8ccee88b5a05310a90dd68a6
naimsiddiquibd <naimsiddiquibd@gmail.com>
2021-08-04 11:45:34 +0600

    varriable-array-conditional

--- multi-conditional.js
+++ multi-conditional.js
@@ -1,7 +1,19 @@
+// And
 var gotJob = true;
 var canRide = true;
 
 if (gotJob == true && canRide == true){
+    console.log('Going to buy a bike');
+}
+else {
+    console.log('Can not buy');
+}
+
+// Or
+var gotJob = true;
+var canRide = false;
+
+if (gotJob == true || canRide == true){
     console.log('Going to buy a bike');
 }
 else {
---
+++ varriable-array-condition.js
@@ -0,0 +1,21 @@
+// Varriable
+var bottleColor = "blue";
+var waterQuantity = 1;
+var isFull = false;
+
+// Array
+var items = ['bottle','mug','paper','pen'];
+items.indexOf('papaer');
+
+var items = ['bottle','mug','paper','pen'];
+items.push('watch');
+items.push('envelop');
+items.pop();
+
+// Conditionals
+if(items.length>=4){
+    console.log('you have to many stuff on your desk');
+}
+else{
+    console.log('you have a clean desk')
+}
