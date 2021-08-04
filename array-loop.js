2809e6b8383b1f763ffbd1249368e11ce50845ab
naimsiddiquibd <naimsiddiquibd@gmail.com>
2021-08-04 19:22:30 +0600

    function

---
+++ array-loop.js
@@ -0,0 +1,11 @@
+var numbers = [43,54,63,23,24,65,87,38,83,43];
+for(var i = 0; i < numbers.length; i++){
+    var elements = numbers[i]
+    console.log(elements);
+}
+
+var items = ['bottle','mobile','watch','pen','laptop'];
+for (var i = 0; i < items.length; i++){
+    var item = items[i];
+    console.log(item);
+}
---
+++ function.js
@@ -0,0 +1,10 @@
+function startFunction(){
+    console.log('work towords the switch');
+    console.log('press the switch');
+}
+
+startFunction()
+
+console.log('eat rice')
+console.log('drink tea')
+startFunction()
