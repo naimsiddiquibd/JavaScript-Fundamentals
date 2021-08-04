28ea77a682b833769e16b952e593f9721b6d0f9a
naimsiddiquibd <naimsiddiquibd@gmail.com>
2021-08-04 13:26:02 +0600

    for-loop

---
+++ for-loop.js
@@ -0,0 +1,21 @@
+for(var roastGiven = 0; roastGiven < 7; roastGiven++){
+    console.log('Give me a roast');
+}
+
+for(var i = 0; i<7; i++){
+    console.log(i);
+}
+
+// Odd Number
+// i+=2
+// i = i + 2
+for (var i = 1; i<=20; i+=2){
+    console.log(i);
+}
+
+// Even Number
+// i+=2
+// i = i + 2
+for (var i = 0; i<=20; i+=2){
+    console.log(i);
+}
