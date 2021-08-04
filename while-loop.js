97c9749115d086fde3074dda01c35c581dbb77da
naimsiddiquibd <naimsiddiquibd@gmail.com>
2021-08-04 12:16:41 +0600

    while-loop

---
+++ while-loop.js
@@ -0,0 +1,8 @@
+var roastGiven = 0;
+while (roastGiven < 7){
+    console.log('Give me roast');
+    // roastGiven = roastGiven + 1;
+    // roastGiven += 1;
+    roastGiven++;
+    console.log(roastGiven);
+}
