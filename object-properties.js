1d99aeecbf6d306f645449f3409079c8a228cfa2
naimsiddiquibd <naimsiddiquibd@gmail.com>
2021-08-05 19:23:29 +0600

    objec t-properties

--- object-properties.js
+++ object-properties.js
@@ -5,3 +5,23 @@ var student = {
 }
 console.log(student);
 console.log(student.name);
+
+var student = {
+    id: 21136694,
+    name: 'Naim Siddiqui',
+    cgpa: 3.75,
+}
+console.log(student);
+console.log(student.name);
+
+student.cgpa = 3.81;
+student['cgpa'] = 3.85;
+
+var student = {
+    id: 21136694,
+    name: 'Naim Siddiqui',
+    cgpa: 3.75,
+}
+// set an object property value
+var modifiedName = 'Naim Siddiqui Prince';
+console.log(modifiedName);
