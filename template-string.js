3940efe91977a53369ddb51f40336c9db341b7e0
naimsiddiquibd <naimsiddiquibd@gmail.com>
2021-08-25 21:16:57 +0600

    template-string

---
+++ template-string.js
@@ -0,0 +1,11 @@
+const putbul = 'Rain over me';
+const bieber = "Baby";
+const jackson = `They do not care`; //Backtick
+
+const first = 'Naim';
+const last = 'Siddiqui';
+const fullOld = first + ' ' + last;
+const fullNew = `My name is: ${first} ${last}`;
+console.log(fullOld);
+console.log(fullNew);
+
