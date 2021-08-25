5e8e183200ebafdc5511f96f738ce037efca15a0
naimsiddiquibd <naimsiddiquibd@gmail.com>
2021-08-25 21:37:00 +0600

    arrow-function

---
+++ arrow-function.js
@@ -0,0 +1,12 @@
+const add = (num1,num2) => num1 + num2;
+const sum = add(18,2);
+
+const multiply = (num1, num2, num3) => num1*num2*num3;
+const result = multiply(2,2,2);
+
+const getName = () => 'Naim Siddiqui';
+const name = getName();
+
+console.log(name);
+console.log(sum);
+console.log(result);
